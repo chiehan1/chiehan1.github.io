f140b7bfe3721db8f3f2877c2b2f0b94f855a5e6
chiehan1 <chieh.an.hsu@gmail.com>
2016-03-07 17:50:11 +0800

    delete function name

--- googleSignInTest/googleSignTest.js
+++ googleSignInTest/googleSignTest.js
@@ -1,23 +1,23 @@
 window.karmapa = function() {
 
-signIn : function signIn(googleUser) {
+  signIn : function (googleUser) {
     // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
     // The ID token you need to pass to your backend:
-  var idToken = googleUser.getAuthResponse()["id_token"];
-  console.log("ID Token: " + idToken);
-},
+    var idToken = googleUser.getAuthResponse()["id_token"];
+    console.log("ID Token: " + idToken);
+  },
 
-signOut : function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-      console.log('User signed out.');
-  });
-}
+  signOut : function () {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log("User signed out.");
+    });
+  }
 
 }();
