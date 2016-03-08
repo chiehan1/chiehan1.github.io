9f7274a6d31b22a351e667ee1bba32568a818c8f
chiehan1 <chieh.an.hsu@gmail.com>
2016-03-08 15:34:03 +0800

    add window karmapa namesplace

--- googleSignInTest/googleSignTest.js
+++ googleSignInTest/googleSignTest.js
@@ -1,4 +1,6 @@
-function signIn(googleUser) {
+window.karmapa = {
+
+  signIn: function(googleUser) {
     // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
@@ -9,18 +11,33 @@ function signIn(googleUser) {
     // The ID token you need to pass to your backend:
   var idToken = googleUser.getAuthResponse()['id_token'];
   console.log('ID Token: ' + idToken);
-}
+  }
+
+};
+
+/*function signIn(googleUser) {
+    // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+  var idToken = googleUser.getAuthResponse()['id_token'];
+  console.log('ID Token: ' + idToken);
+}*/
 
 (function() {
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-}
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 
-document.getElementById('signOutLink')
-  .addEventListener('click', signOut, false);
+  document.getElementById('signOutLink')
+    .addEventListener('click', signOut, false);
 
 })();
